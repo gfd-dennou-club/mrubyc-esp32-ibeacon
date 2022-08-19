31809ec58964ae1e968bfad85c274c68092bf486
sugiymki <sugiyama@gfd-dennou.org>
2022-08-19 12:24:50 +0900

    update

--- main/mrbc_esp32_ibeacon.c
+++ main/mrbc_esp32_ibeacon.c
@@ -1,28 +1,8 @@
-/*
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
-
-
-
-/****************************************************************************
-*
-* This file is for iBeacon demo. It supports both iBeacon sender and receiver
-* which is distinguished by macros IBEACON_SENDER and IBEACON_RECEIVER,
-*
-* iBeacon is a trademark of Apple Inc. Before building devices which use iBeacon technology,
-* visit https://developer.apple.com/ibeacon/ to obtain a license.
-*
-****************************************************************************/
-
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
 #include <stdio.h>
-//#include "nvs_flash.h"
+#include "nvs_flash.h"
 
 #include "esp_bt.h"
 #include "esp_gap_ble_api.h"
@@ -30,7 +10,6 @@
 #include "esp_gatt_defs.h"
 #include "esp_bt_main.h"
 #include "esp_bt_defs.h"
-#include "esp_ibeacon_api.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 
@@ -40,15 +19,94 @@
 #include "mrubyc.h"
 
 static const char* DEMO_TAG = "IBEACON_DEMO";
-extern esp_ble_ibeacon_vendor_t vendor_config;
-
-/*
-int   rssi;
-float dist;
-uint16_t major;
-uint16_t minor;
-*/
 int info[4];
+
+////////////////////////////////////////////////////////////////////////////
+
+/* Because current ESP IDF version doesn't support scan and adv simultaneously,
+ * so iBeacon sender and receiver should not run simultaneously */
+#define IBEACON_SENDER      0
+#define IBEACON_RECEIVER    1
+#define IBEACON_MODE 1           // Default mode is "receiver"
+
+/* Espressif WeChat official account can be found using WeChat "Yao Yi Yao Zhou Bian", 
+ * if device advertises using ESP defined UUID. 
+ * Please refer to http://zb.weixin.qq.com for further information. */
+#define ESP_UUID    {0xFD, 0xA5, 0x06, 0x93, 0xA4, 0xE2, 0x4F, 0xB1, 0xAF, 0xCF, 0xC6, 0xEB, 0x07, 0x64, 0x78, 0x25}
+#define ESP_MAJOR   11111
+#define ESP_MINOR   22222
+
+/* Major and Minor part are stored in big endian mode in iBeacon packet,
+ * need to use this macro to transfer while creating or processing
+ * iBeacon data */
+#define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00)>>8) + (((x)&0xFF)<<8))
+
+typedef struct {
+    uint8_t flags[3];
+    uint8_t length;
+    uint8_t type;
+    uint16_t company_id;
+    uint16_t beacon_type;
+}__attribute__((packed)) esp_ble_ibeacon_head_t;
+
+typedef struct {
+    uint8_t proximity_uuid[16];
+    uint16_t major;
+    uint16_t minor;
+    int8_t measured_power;
+}__attribute__((packed)) esp_ble_ibeacon_vendor_t;
+
+typedef struct {
+    esp_ble_ibeacon_head_t ibeacon_head;
+    esp_ble_ibeacon_vendor_t ibeacon_vendor;
+}__attribute__((packed)) esp_ble_ibeacon_t;
+
+////////////////////////////////////////////////////////////////////////////
+
+const uint8_t uuid_zeros[ESP_UUID_LEN_128] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+/* For iBeacon packet format, please refer to Apple "Proximity Beacon Specification" doc */
+/* Constant part of iBeacon data */
+esp_ble_ibeacon_head_t ibeacon_common_head = {
+    .flags = {0x02, 0x01, 0x06},
+    .length = 0x1A,
+    .type = 0xFF,
+    .company_id = 0x004C,
+    .beacon_type = 0x1502
+};
+
+/* Vendor part of iBeacon data*/
+esp_ble_ibeacon_vendor_t vendor_config = {
+    .proximity_uuid = ESP_UUID,
+    .major = ENDIAN_CHANGE_U16(ESP_MAJOR), //Major=ESP_MAJOR
+    .minor = ENDIAN_CHANGE_U16(ESP_MINOR), //Minor=ESP_MINOR
+    .measured_power = 0xC5
+};
+
+bool esp_ble_is_ibeacon_packet (uint8_t *adv_data, uint8_t adv_data_len){
+    bool result = false;
+
+    if ((adv_data != NULL) && (adv_data_len == 0x1E)){
+        if (!memcmp(adv_data, (uint8_t*)&ibeacon_common_head, sizeof(ibeacon_common_head))){
+            result = true;
+        }
+    }
+
+    return result;
+}
+
+esp_err_t esp_ble_config_ibeacon_data (esp_ble_ibeacon_vendor_t *vendor_config, esp_ble_ibeacon_t *ibeacon_adv_data){
+    if ((vendor_config == NULL) || (ibeacon_adv_data == NULL) || (!memcmp(vendor_config->proximity_uuid, uuid_zeros, sizeof(uuid_zeros)))){
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    memcpy(&ibeacon_adv_data->ibeacon_head, &ibeacon_common_head, sizeof(esp_ble_ibeacon_head_t));
+    memcpy(&ibeacon_adv_data->ibeacon_vendor, vendor_config, sizeof(esp_ble_ibeacon_vendor_t));
+
+    return ESP_OK;
+}
+
+////////////////////////////////////////////////////////////////////////////
 
 ///Declare static functions
 static void esp_gap_cb(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
@@ -189,9 +247,11 @@ void ble_ibeacon_init(void)
 }
 */
 
+////////////////////////////////////////////////////////////////////////////
+
 void ble_ibeacon_init(void)
 {
-  //    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
     esp_bt_controller_init(&bt_cfg);
--- samples/master.rb
+++ samples/master.rb
@@ -1,70 +1,15 @@
 
 ibeacon = IBeacon.new 
-
-led13 = GPIO.new( 13, GPIO::OUT )
-led12 = GPIO.new( 12, GPIO::OUT )
-led14 = GPIO.new( 14, GPIO::OUT )
-led27 = GPIO.new( 27, GPIO::OUT )
-led26 = GPIO.new( 26, GPIO::OUT )
-led25 = GPIO.new( 25, GPIO::OUT )
-led33 = GPIO.new( 33, GPIO::OUT )
-led32 = GPIO.new( 32, GPIO::OUT )
-
-i2c = I2C.new(22, 21)
-lcd = AQM0802A.new(i2c)
-
-sw34 = GPIO.new( 34, GPIO::IN, GPIO::PULL_UP)
-sw35 = GPIO.new( 35, GPIO::IN, GPIO::PULL_UP)
-sw18 = GPIO.new( 18, GPIO::IN, GPIO::PULL_UP)
-sw19 = GPIO.new( 19, GPIO::IN, GPIO::PULL_UP)
-
-pwm1 = PWM.new( 15 )
-
 while true do
   ibeacon.get
-  if ibeacon.major == 11111
-    if ibeacon.dist < 10
-      led13.write(1)
-      led12.write(0)
-      led14.write(1)
-      led27.write(0)
-      led26.write(1)
-      led25.write(0)
-      led33.write(1)
-      led32.write(1)
-      lcd.cursor(0, 0)
-      lcd.write_string("        ")
-      lcd.cursor(0, 0)
-      lcd.write_string((ibeacon.minor).to_s)
-      lcd.cursor(0, 1)
-      lcd.write_string("        ")
-      lcd.cursor(0, 1)
-      lcd.write_string((ibeacon.dist).to_s)
-      if ibeacon.dist < 1
-        led13.write(1)
-        led12.write(1)
-        led14.write(1)
-        led27.write(1)
-        led26.write(1)
-        led25.write(1)
-        led33.write(1)
-        led32.write(1)
-        if (sw34.read == 0) && (sw35.read == 0) && (sw18.read == 0) && (sw19.read == 1)
-          pwm1.freq(261)
-          pwm1.duty(512)
-        end
-      end
-    end
-    sleep(1)
-  end
-  led13.write(0)
-  led12.write(0)
-  led14.write(0)
-  led27.write(0)
-  led26.write(0)
-  led25.write(0)
-  led33.write(0)
-  led32.write(0)
-  pwm1.duty(0)
+#  if ibeacon.major == 11111
+#    puts "minor : #{ibeacon.minor}"
+#    puts "#{ibeacon.dist} m"
+#  end
+
+  puts "major : #{ibeacon.major}"
+  puts "minor : #{ibeacon.minor}"
+  puts "#{ibeacon.dist} m"
+
   sleep(0.05)
 end
